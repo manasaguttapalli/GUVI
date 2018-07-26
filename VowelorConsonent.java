fee21c025ad6746e1071e50aa90d614c1db469db
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-07-26 22:15:56 +0530

    Create VowelorConsonent.java

---
+++ VowelorConsonent.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class VowelorConsonent {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		char c=s.next().charAt(0);
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+			System.out.println("Vowel");
+		}
+		else if(c>'a' && c<'z'){
+			System.out.println("Consonent");
+		}
+		else{
+			System.out.println("invalid");
+		}
+
+	}
+
+}
