6748227dc2749efc7ad4e7345c2e1516e75a9e4c
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-07-27 11:50:35 +0530

    Create RepeatAString.java

---
+++ RepeatAString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class RepeatAString {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n<=0){
+			System.out.println(" ");
+		}
+		for(int i=0;i<n;i++){
+			System.out.println("Hello");
+		}
+			
+	}
+
+}
