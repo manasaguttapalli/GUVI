6bb9b7651b8f1914797d76732ae9e02aedbadcb5
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-07-29 12:13:08 +0530

    Update ElementsCount.java

--- ElementsCount.java
+++ ElementsCount.java
@@ -4,7 +4,7 @@ public class ElementsCount {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		int rem,i=0;
+		int i=0;
 		while(n!=0){
 			n=n/10;
 			i++;
