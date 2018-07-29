4a03fe0e6653df1ced68d12192784a2d1e0c6324
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-07-29 23:29:15 +0530

    Create OddsBetweenBoundaries.java

---
+++ OddsBetweenBoundaries.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class oddNumbersIntervals {
+
+	public static void main(String[] args) {
+	 Scanner s=new Scanner(System.in);
+	 int N=s.nextInt();
+	 int Q=s.nextInt();
+	 if(N==Q+1 || Q==N+1){
+		 System.out.println("no primes exist");
+	 }else{
+	 if(N<Q){
+	 for(int i=N+1;i<Q;i++){
+		  if(i%2!=0){
+			 System.out.print(i+" ");
+			 
+		 }
+	 }
+	}else{
+		for(int i=Q+1;i<N;i++){
+			  if(i%2!=0){
+				 System.out.print(i+" ");
+			  }
+		}
+	
+
+		}
+	 
+	 }
+	}
+}
