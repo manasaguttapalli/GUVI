4bde6d3949503f1d7847bed767bea746298a53bf
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-02 13:40:52 +0530

    Create MinimumOfArray.java

---
+++ MinimumOfArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class minimumOfArray {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int max;
+		int N=s.nextInt();
+		int a[]=new int[N];
+		for(int i=0;i<N;i++){
+			a[i]=s.nextInt();
+		}
+		max=a[0];
+		for(int i=0;i<N;i++){
+			
+			if(max>a[i]){
+				max=a[i];
+			}
+		}
+		System.out.print(max);
+	}
+
+}
