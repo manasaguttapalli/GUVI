9fa5d5a899ed5fa5d7444b76e179116aaf01116a
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-09 13:34:38 +0530

    Create NoOfWords.java

---
+++ NoOfWords.java
@@ -0,0 +1,16 @@
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+				int num=1;
+				for(int i=0;i<str.length();i++){
+					if(str.charAt(i)==' '){
+						num++;
+					}
+			
+				}
+		
+				System.out.println(num);
+	}
+
+}
