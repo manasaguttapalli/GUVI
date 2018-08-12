906d2b7c37e0beeb930d61f29b14307da6554c64
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-12 20:02:29 +0530

    Create SpecialCharecters.java

---
+++ SpecialCharecters.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SpecialCharectersInString {
+public static void main(String args[]){
+	Scanner s=new Scanner(System.in);
+	int count=0;
+	String str=s.nextLine();
+	for(int i=0;i<str.length();i++){
+
+		if(str.charAt(i)>='0' && str.charAt(i)<='9' || str.charAt(i)>='a' && str.charAt(i)<='z' || str.charAt(i)>='A' && str.charAt(i)<='Z' || str.charAt(i)==' '){
+			continue;
+		}
+		else{
+			count++;
+		}
+	
+	}
+	System.out.println(count);
+}
+
+}
