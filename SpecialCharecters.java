bbb86a1dc047e17d5325249e5844ed86407ee50c
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-12 20:25:20 +0530

    Update SpecialCharecters.java

--- SpecialCharecters.java
+++ SpecialCharecters.java
@@ -6,7 +6,7 @@ public static void main(String args[]){
 	String str=s.nextLine();
 	for(int i=0;i<str.length();i++){
 
-		if(str.charAt(i)>='0' && str.charAt(i)<='9' || str.charAt(i)>='a' && str.charAt(i)<='z' || str.charAt(i)>='A' && str.charAt(i)<='Z' || str.charAt(i)=='_'){
+		if(str.charAt(i)>='0' && str.charAt(i)<='9' || str.charAt(i)>='a' && str.charAt(i)<='z' || str.charAt(i)>='A' && str.charAt(i)<='Z'){
 			continue;
 		}
 		else{
