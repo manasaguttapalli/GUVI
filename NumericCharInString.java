79782be46ff832f2f34d76416dcf3b89f8f723bc
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-12 18:41:04 +0530

    Create NumericCharInString.java

---
+++ NumericCharInString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class NoofNumChars {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		int count=0;
+		for(int i=0;i<str.length();i++){
+		if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+			count++;
+		}
+		}
+		System.out.println(count);	
+	}
+
+}
