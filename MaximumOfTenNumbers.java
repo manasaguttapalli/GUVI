928963abfcfaf3a681c946cf8dee9904cc9e594c
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-13 10:34:48 +0530

    Create MaximumOfTenNumbers.java

---
+++ MaximumOfTenNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MaximumOfTenNumbers {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int max;
+		int a[]=new int[10];
+		for(int i=0;i<10;i++){
+			a[i]=s.nextInt();
+		}
+		max=a[0];
+		for(int i=0;i<a.length;i++){
+			
+			if(max<a[i]){
+				max=a[i];
+			}
+		}
+		System.out.print(max);
+	}
+
+}
