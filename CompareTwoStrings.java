b95a0af166efbab9c355206ca57096c600de821f
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-13 13:52:39 +0530

    Create CompareTwoStrings.java

---
+++ CompareTwoStrings.java
@@ -0,0 +1,13 @@
+if(name1.length()>name2.length())
+	{
+		System.out.println(name1);
+	}
+	else if(name1.contentEquals(name2))
+	{
+		System.out.println(name2);	
+		
+	}
+	else if(name1.length()<name2.length())
+	{
+		System.out.println(name2);
+	}
