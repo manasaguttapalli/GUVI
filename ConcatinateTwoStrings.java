b21e8135e1a6b719885871d28643babd0a73c410
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-15 11:56:02 +0530

    Create ConcatinateTwoStrings.java

---
+++ ConcatinateTwoStrings.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ConcatinateTwoStrings {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str1=s.next();
+		String str2=s.next();
+		String str3=str1+str2;
+		System.out.println(str3);
+
+	}
+
+}
