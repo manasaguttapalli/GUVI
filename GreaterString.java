33329b98257f1892878d30f435052fd56ece3d48
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-16 13:53:44 +0530

    Create GreaterString.java

---
+++ GreaterString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class GreaterString {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str1=s.next();
+		String str2=s.next();
+		 if(str1.length()>str2.length())
+		{
+			System.out.println(str1);
+		}
+		else 
+		{
+			System.out.println(str2);
+		}
+			
+		
+
+}
+}
