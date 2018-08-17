16e61a60e472c0e6fdda2297c6424db10d92816a
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-17 13:10:01 +0530

    Create NumOccurances.java

---
+++ NumOccurances.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class NoOfoccurances {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int M=s.nextInt();
+		int count=0;
+		int a[]=new int[N];
+		for(int i=0;i<N;i++){
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<a.length;i++){
+			if(M==a[i]){
+				count++;
+			}
+		}
+		System.out.println(count);
+	}	
+}
