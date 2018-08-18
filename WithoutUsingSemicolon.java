bdf45cba57fc1263b972dc544c2e96478f7106bb
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-18 10:18:48 +0530

    Create WithoutUsingSemicolon.java

---
+++ WithoutUsingSemicolon.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+        int i = 0;
+        if(System.out.printf(str) == null) {}
+       
+     }
+}
