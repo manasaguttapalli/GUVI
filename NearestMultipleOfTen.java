8f06fb46dc2526a48a9121d63b52418e54ab68e3
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-18 10:57:45 +0530

    Create NearestMultipleOfTen.java

---
+++ NearestMultipleOfTen.java
@@ -0,0 +1,18 @@
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%10==0){
+			System.out.println(n);
+		}else{
+		int a = (n / 10) * 10;
+		int b = a + 10;
+		System.out.println(b);
+			
+		}
+		
+	}
+}
