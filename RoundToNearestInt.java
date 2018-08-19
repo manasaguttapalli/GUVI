bb8265530df402cf0f1060ab626f9d429436b03f
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-19 20:54:34 +0530

    Create RoundToNearestInt.java

---
+++ RoundToNearestInt.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class RoundToGreaterInteger {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		float n=s.nextFloat();
+		int m=(int) n;
+		System.out.println(m+1);
+
+	}
+
+}
