f57bdb1c713a14f53cd223fdae279f78270e452f
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-19 20:37:08 +0530

    Create HasVowelsOrNot.java

---
+++ HasVowelsOrNot.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class HasVowelOrNot {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int count=0;
+		String str=s.nextLine();
+		for(int i=0;i<str.length();i++){
+		if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
+			count=1;
+		}
+		}
+		if(count>0){
+			System.out.println("yes");
+		}else{
+			System.out.println("no");
+		}
+	}
+
+}
