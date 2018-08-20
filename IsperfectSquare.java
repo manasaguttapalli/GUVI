2e40f83434738134a4d7e51b14470672463167b1
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-20 23:17:49 +0530

    Create IsperfectSquare.java

---
+++ IsperfectSquare.java
@@ -0,0 +1,23 @@
+public class ProductIsPerfectsquare {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		double pro=a*b;
+		if(isPerfectSquare(pro)){
+			System.out.println("yes");
+		}else{
+			System.out.println("no");
+		}
+	}
+
+
+static boolean isPerfectSquare(double x) 
+{
+    double sr = Math.sqrt(x);
+ 
+    
+    return ((sr - Math.floor(sr)) == 0);
+	}
+}
