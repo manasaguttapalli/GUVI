3d20e8603f4d9336acc850d01c3972eebe0246d1
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-29 21:00:20 +0530

    Create RoundUptoFiveDigits.java

---
+++ RoundUptoFiveDigits.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AreaUpto5Digits {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		double a=s.nextFloat();
+		double b=s.nextFloat();
+		double c=a*b;
+		System.out.format("%4f",c);
+	}
+	
+		}
