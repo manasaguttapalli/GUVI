6b839d70daf3c7dadb1e233dd3ccb13b19fd08f8
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-08-30 10:53:53 +0530

    Update AsciiValues.java

--- AsciiValues.java
+++ AsciiValues.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
+public class AsciiValue {
 
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		char a=s.next().charAt(0);
+		int i=(char)a;
+		System.out.println(i);
+
+	}
+
+}
