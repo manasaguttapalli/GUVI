52c26479e7828a0f46d407fa086ef671e1493180
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-09-09 16:45:39 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int p=s.nextInt();
+		int t=s.nextInt();
+		int r=s.nextInt();
+		int si=(p*t*r)/100;
+		System.out.println(si);
+	}
+
+}
