65e826a50ade648dc674e8ef7acacc3beb0a0611
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-09-09 16:50:59 +0530

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -6,8 +6,13 @@ public class SimpleInterest {
 		int p=s.nextInt();
 		int t=s.nextInt();
 		int r=s.nextInt();
+		if(p<0||t<0||r<0){
+			System.out.println("negative values are not allowed");
+		}
+		else{
 		int si=(p*t*r)/100;
 		System.out.println(si);
+		}
 	}
 
 }
