1a11b71f4c9437076b48baf7c16542d22b0feea9
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-09-09 16:28:48 +0530

    Create CelsiusTokelvin.java

---
+++ CelsiusTokelvin.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CelvinToKelvin {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int celsius=s.nextInt();
+		int kelvin=celsius+273;
+		System.out.println(kelvin);
+
+	}
+
+}
