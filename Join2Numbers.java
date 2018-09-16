77620cdd332ded823c4e6f16a74cc59baedcdbbc
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-09-16 18:48:16 +0530

    Create Join2Numbers.java

---
+++ Join2Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Join2Numbers {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String num1=s.next();
+		String num2=s.next();
+		System.out.print(num1+num2);
+
+	}
+
+}
