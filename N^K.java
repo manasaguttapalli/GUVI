675c3e21cd6a7300dfe3e0b5edd2752df5a8e05c
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-09-16 18:39:29 +0530

    Create N^K.java

---
+++ N^K.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.math.*;
+public class N^K {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int p=s.nextInt();
+		int result=(int) Math.pow(n, p);
+		System.out.println(result);
+		
+
+	}
+
+}
