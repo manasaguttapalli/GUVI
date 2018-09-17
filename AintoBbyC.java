3369e7f5b82821861dd59d691e094b862712b489
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-09-17 21:33:49 +0530

    Create AintoBbyC.java

---
+++ AintoBbyC.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AintoBmodC {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		int ans=(a*b)/c;
+		System.out.println(ans);
+				
+
+	}
+
+}
