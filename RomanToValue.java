b83f13ba36b11915e1198cccdec520b52fac4e52
manasaguttapalli <41725866+manasaguttapalli@users.noreply.github.com>
2018-09-25 10:59:56 +0530

    Create RomanToValue.java

---
+++ RomanToValue.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class RomanToValue {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		RomanToValue ob=new RomanToValue();
+		System.out.println(ob.romanmethod(str));		
+		
+	}
+
+	int romanmethod(String str) {
+		int res=0;
+		for(int i=0;i<str.length();i++){
+			int s1=value(str.charAt(i));
+			if(i+1<str.length()){
+			int s2=value(str.charAt(i+1));
+			if(s1>=s2){
+				res=res+s1;
+			}
+			else{
+			res=res+s2-s1;
+			i++;
+			
+			}
+			}
+			else{
+				res=res+s1;
+				i++;
+			}
+		}
+		return res;
+		
+	}
+
+
+	private int value(char r) {
+		if(r=='I')
+			return 1;
+		 if(r=='V')
+				return 5;
+		 if(r=='X')
+				return 10;
+		 if(r=='L')
+				return 50;
+		 if(r=='C')
+			 return 100;
+				return -1;
+	}
+
+}
